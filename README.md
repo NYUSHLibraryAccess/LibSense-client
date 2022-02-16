66039f6b6fc3ffd457ba71dba08259e0af129135
Liyang Zhu <1184997399@qq.com>
2022-02-16 21:31:45 +0800

    :sparkles: initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# NYU Shanghai Library WMS
+
+This is the repository for the frontend of the NYU Shanghai Library Workflow Management System.
+
+## How To Use
+
+To install dependencies and build, run:
+
+```bash
+yarn install
+yarn run build
+```
+
+To start the development server, run:
+
+```bash
+yarn run start
+```
---
+++ utils/start.js
@@ -0,0 +1,39 @@
+// Do this as the first thing so that any code reading it knows the right env.
+process.env.BABEL_ENV = 'development';
+process.env.NODE_ENV = 'development';
+
+const path = require('path');
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const env = require('./env');
+const config = require('../webpack.config');
+
+(async () => {
+  for (let entryName in config.entry) {
+    config.entry[entryName] = [
+      `webpack-dev-server/client?hostname=${env.HOST}&port=${env.PORT}&hot=true`,
+      "webpack/hot/dev-server",
+    ].concat(config.entry[entryName]);
+  }
+
+  const compiler = webpack(config);
+
+  const server = new WebpackDevServer({
+    hot: false,
+    liveReload: false,
+    client: false,
+    static: path.resolve(__dirname, "../build"),
+    devMiddleware: {
+      writeToDisk: true
+    },
+    host: env.HOST,
+    port: env.PORT,
+  }, compiler);
+
+  await server.start();
+  ["SIGINT", "SIGTERM", "SIGQUIT"].forEach(signal => process.on(signal, () => {
+    compiler.close(() => {
+    });
+    process.exit();
+  }));
+})();
